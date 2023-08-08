d03ec463c587c40c3145b06487373f4349348dc6
NameSasipha <sasipha_th@cmu.ac.th>
2023-08-09 00:11:30 +0700

    fix

--- namesasipha/src/App.vue
+++ namesasipha/src/App.vue
@@ -9,7 +9,7 @@ const { message } = storeToRefs(store)
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
       <nav>
